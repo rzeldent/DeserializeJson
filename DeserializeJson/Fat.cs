3a693de2c696fa52a49d2c5bbd93d9685edd9985
Rene Zeldenthuis <rzeldent@outlook.com>
2025-04-12 12:38:42 +0200

    Add project files.

---
+++ DeserializeJson/Fat.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+
+namespace DeserializeJson
+{
+    internal struct Fat
+    {
+        public string Process { get; set; }
+        public Dictionary<string, JsonElement> Parameters { get; set; }
+    }
+}
---
+++ DeserializeJson/Program.cs
@@ -0,0 +1,20 @@
+﻿using DeserializeJson;
+using System.Drawing;
+using System.Text.Json;
+
+var jsonOptions = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true,
+    WriteIndented = true,
+    ReadCommentHandling = JsonCommentHandling.Skip
+};
+
+var json = File.ReadAllText("fat.json");
+
+var fat = JsonSerializer.Deserialize<Fat>(json, jsonOptions);
+
+var timeout = fat.Parameters["Timeout"].Deserialize<TimeSpan>();
+Console.WriteLine($"Timeout: {timeout}");
+
+var size = fat.Parameters["Size"].Deserialize<Size>();
+Console.WriteLine($"Size: {size}");
